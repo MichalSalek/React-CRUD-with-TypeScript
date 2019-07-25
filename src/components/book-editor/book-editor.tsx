9712963bc59d3506b350c875b6603f40fb800c66
MichalSalek <michal.salek@abventor.com>
2019-07-25 18:22:11 +0200

    Content of a book from API -> state hook, waiting with render component on call resolve added

--- src/components/book-editor/book-editor.tsx
+++ src/components/book-editor/book-editor.tsx
@@ -9,81 +9,104 @@ import http from '../../http.service';
 // import { BookApiCollection, BookApiItem } from '../../domainModel';
 // import { IDataPreparedForTable } from './books-list.model';
 
+interface IGetResponse {
+  status: number;
+}
+
 const BookEditor = (props: any) => {
   const { match } = props;
   const { url } = match;
 
-  const [titleF, setTitle] = useState('Loading...');
+  const [callResolve, setCallResolve] = useState(false);
+  const [titleState, setTitleState] = useState('Loading...');
+  const [ISBNState, setISBNState] = useState('Loading...');
+  const [descriptionState, setDescriptionState] = useState('Loading...');
+  const [authorState, setAuthorState] = useState('Loading...');
 
   useEffect(() => {
-    console.warn(props.store);
-    setEditorOpen(true);
+    props.setEditorOpen(true);
   }, []);
 
   useEffect(() => {
-    setCurrentTitle(titleF);
-  }, [titleF]);
+    props.setCurrentTitle(titleState);
+  }, [titleState]);
 
   const callForSingleBook = (path: string) => {
     http
       .get(path)
       .then(result => {
+        const statusOfResponse = result.status;
+        if (!statusOfResponse) {
+          return null;
+        }
+        const bookRawData = result.data.data.attributes;
         console.dir(result);
-        setTitle(result.data.data.attributes.title);
+        setTitleState(bookRawData.title);
+        setISBNState(bookRawData.isbn);
+        setDescriptionState(bookRawData.description);
+        setAuthorState(bookRawData.author);
+        setCallResolve(true);
+        return null;
       })
       .catch(error => console.error(error));
+    return null;
   };
 
   useEffect(() => {
     callForSingleBook(url);
   }, [url]);
 
+  useEffect(() => console.warn(props.store));
+
   return (
     <React.Fragment>
-      <Formik
-        initialValues={{ title: titleF }}
-        validate={values => {
-          const errors = {
-            title: '',
-          };
-          if (!values.title) {
-            errors.title = 'Required';
-          }
-          return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="title"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.title}
-            />
-            {errors.title && touched.title && errors.title}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Formik>
-      <button onClick={() => setEditorOpen(true)}>click</button>
+      {callResolve ? (
+        <Formik
+          initialValues={{ title: titleState }}
+          validate={values => {
+            const errors = {
+              title: '',
+            };
+            if (!values.title) {
+              errors.title = 'Required';
+            }
+            return errors;
+          }}
+          onSubmit={(values, { setSubmitting }) => {
+            setTimeout(() => {
+              alert(JSON.stringify(values, null, 2));
+              setSubmitting(false);
+            }, 400);
+          }}
+        >
+          {({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            isSubmitting,
+            /* and other goodies */
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                name="title"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.title}
+              />
+              {errors.title && touched.title && errors.title}
+              <button type="submit" disabled={isSubmitting}>
+                Submit
+              </button>
+            </form>
+          )}
+        </Formik>
+      ) : (
+        'Loading your book info...'
+      )}
     </React.Fragment>
   );
 };
